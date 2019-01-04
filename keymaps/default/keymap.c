f7d31c781eb5cb36f2e5a0174c352d2b9741fa8f
Bernard (Work) <bernardshih@tencent.com>
2019-01-04 11:53:30 +0800

    20190104

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT( \
   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  LGUI_T(KC_TAB),   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   LCTL(KC_ENT), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
-                        LOWER, KC_BSPC, KC_LCTRL,   KC_ENT,  KC_SPC, RAISE \
+                        LOWER, LALT_T(KC_BSPC), LCTL_T(KC_ESC),   LT(2, LALT(KC_LSFT)), KC_ENT,  KC_SPC \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
 KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  \
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_DEL,  KC_HOME,  KC_UP,   KC_END,  _______, _______, \
-  _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_LBRC,                   KC_RBRC, KC_LEFT,  KC_DOWN, KC_RGHT, KC_SCLN, _______, \
-  _______, _______, _______, RESET, _______, _______,  _______, LCTL(KC_INS), _______, _______, _______, _______,\
+  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_DEL,  KC_HOME,  KC_UP,   KC_END,  _______, _______, \
+  RESET, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQL,  KC_LBRC,                   KC_RBRC, KC_LEFT,  KC_DOWN, KC_RGHT, KC_SCLN, _______, \
+  _______, _______, _______, _______, KC_MINS, _______,  _______, _______, LCTL(KC_INS), _______, _______, _______,\
                               _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
