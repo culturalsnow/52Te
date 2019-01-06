b859106bf65813360c83c7d5fb7c1feebf03e988
Bernard Shih <bernard@Stormrage.local>
2019-01-06 22:22:09 +0800

    keymap updated

--- rev1/config.h
+++ rev1/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CATERINA_BOOTLOADER
 
 /* define tapping term */
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
--- rev1/rev1.h
+++ rev1/rev1.h
@@ -30,11 +30,11 @@
 		{ L20, L21, L22, L23, L24, L25 }, \
 		{ L30, L31, L32, L33, L34, L35 }, \
 		{ KC_NO, KC_NO, KC_NO, L40, L41, L42 }, \
-		{ R04, R03, R02, R01, R00, KC_NO }, \
-		{ R15, R14, R13, R12, R11, R10 }, \
-		{ R25, R24, R23, R22, R21, R20 }, \
-		{ R35, R34, R33, R32, R31, R30 }, \
-		{ R42, R41, R40, KC_NO, KC_NO, KC_NO } \
+		{ R00, R01, R02, R03, R04, KC_NO }, \
+		{ R10, R11, R12, R13, R14, R15 }, \
+		{ R20, R21, R22, R23, R24, R25 }, \
+		{ R30, R31, R32, R33, R34, R35 }, \
+		{ R40, R41, R42, KC_NO, KC_NO, KC_NO } \
 	}
 #else
 // Keymap with right side flipped
